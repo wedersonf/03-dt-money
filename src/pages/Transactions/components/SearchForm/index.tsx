77f01bf764e20cc56626f3b77e9c156f8246066e
wedersonf <wedymf@hotmail.com>
2023-07-22 03:48:14 -0300

    feat: get filtered data

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,9 +1,11 @@
+import { useContext } from "react";
 import { MagnifyingGlass } from "@phosphor-icons/react";
 import { useForm } from "react-hook-form";
 import * as z from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 
 import { SearchFormContainer } from "./styles";
-import { zodResolver } from "@hookform/resolvers/zod";
 
 const searchFormSchema = z.object({
   query: z.string()
@@ -12,6 +14,8 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext)
+
   const { 
     register,
     handleSubmit,
@@ -21,9 +25,7 @@ export function SearchForm() {
   })
 
   async function handleSearchTransactions(data: SearchFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 2000))
-
-    console.log(data)
+    await fetchTransactions(data.query)
   }
   
   return (
